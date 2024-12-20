ed7cca674b3b894232d38ea1828c7aeb0989143d
KHSenevirathne <khsenevirathne@gmail.com>
2024-12-20 08:23:12 +0530

    first

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo{
+    public static void main(String[] args) {
+        //beginner level
+        System.out.println();
+        System.out.println("Hello World");
+        System.out.println("A");
+        System.out.print("B");
+        System.out.println("C");
+        System.out.println("My name is \"Kaveesh\"");
+    }
+}
